71b24b4e6d76b492defe1efd86c5b699f5f6d63c
Lucía Cerutti <78547668+Lou-27@users.noreply.github.com>
2021-03-09 15:52:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.banner');
+ScrollReveal().reveal('.titulos', { delay: 500 });
 ScrollReveal().reveal('.news-cards', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
