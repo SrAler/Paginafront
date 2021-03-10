78879cdf2b3b81c56641ef2c10ac2220f35a62c9
SrAler <79024987+SrAler@users.noreply.github.com>
2021-03-10 13:04:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.news-cards', { delay: 500 });
-ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
-ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+ScrollReveal().reveal('.banner');
+ScrollReveal().reveal('.titulos', { delay: 500 });
+ScrollReveal().reveal('.servicios-cards', { delay: 500 });
+/*ScrollReveal().reveal('.footer-container', { delay: 500 });*/
+ScrollReveal().reveal('.profesionales-cards', { delay: 500 });
